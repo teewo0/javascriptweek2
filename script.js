33a484f52cb37eeb8479976a3933396c83390b7a
teewo0 <teewodavie@gmail.com>
2021-06-25 12:00:17 +0100

    javascript

---
+++ script.js
@@ -0,0 +1,75 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed"
+}
+
+document.write("teewo");
+
+
+console.log(900);
+
+var x = 5;
+var y = 6;
+var y = 10;
+var z = x + y;
+
+console.log(z)
+
+var name = 'teewo'; 
+var age = 19;
+var rightWrong = true;
+var numChildren = null;
+var definition;
+
+console.log(definition)
+
+
+
+var city = 'asaba';
+var age = 60;
+var rightWrong = false;
+var numChildren = 20;
+var definition = undefined;
+
+console.log(city)
+console.log(age)
+console.log(rightWrong)
+console.log(numChildren)
+console.log(definition)
+
+document.getElementById("city").innerHTML = "asaba"
+document.getElementById("age").innerHTML = "60"
+document.getElementById("rightWrong").innerHTML = "false"
+document.getElementById("numChildren").innerHTML = "20"
+document.getElementById("definition").innerHTML = "undefined"
+
+
+var number1 = 10;
+var number2 = 20;
+
+var sum = number1 + number2;
+console.log(sum)
+
+var a = 30;
+var b = 50;
+
+var multiple = a * b;
+console.log(multiple);
+
+var h = 30;
+var i = 15;
+
+var subt = h - i;
+console.log(subt);
+
+var m = 65;
+var n = 5;
+var division = m/n;
+console.log(division);
+
+var s = 10;
+s++;
+console.log(s);
+
+var t = 50;
+t--;
+console.log(t);
